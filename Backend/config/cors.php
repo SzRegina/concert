744c19decd23f3f9e9297ddaac1fa2dbe632c7ed
SzRegina <regina.szeprethy@gmail.com>
2026-04-21 09:52:49 +0200

    "new deployment"

--- Backend/config/cors.php
+++ Backend/config/cors.php
@@ -20,9 +20,8 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-    env('FRONTEND_URL', 'http://localhost:3000',
-  'http://127.0.0.1:3000'),
-],
+      env('FRONTEND_URL', 'http://localhost:3000'),
+      'https://seaty-deploy.netlify.app',],
 
     'allowed_origins_patterns' => [],
 
