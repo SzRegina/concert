7427e3acf38d8adf0d4523965a155a6a551851c7
SzRegina <regina.szeprethy@gmail.com>
2026-04-21 10:12:46 +0200

    "new deployment2"

--- frontend/src/utility/config.tsx
+++ frontend/src/utility/config.tsx
@@ -1,4 +1,1 @@
-export const API_BASE =
-  process.env.REACT_APP_API_BASE ?? "http://127.0.0.1:8000";
-
-console.log("API_BASE =", API_BASE);
+export const REACT_APP_API_BASE="https://seaty-deploy.netlify.app";
